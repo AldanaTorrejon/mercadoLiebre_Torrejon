9027988c9053539f38a9c6961d0643ca88526ba9
DESKTOP-TQPHTIM\net/Aldana <aldanacarp2017@gmail.com>
2023-08-09 00:05:14 -0300

    update proyecto con render

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ const path = require("path");
 
 app.use(express.static("./public")) //express sabe donde se encuentran archivos estaticos
 
-app.listen(3030, () => console.log("servidor corriendo"));
+const port= process.env.PORT || 3030;
+app.listen(port,()=>console.log(`Servidor corriendo en el puerto${port}`));cle
 
 app.get("/", (req,res) =>{
     res.sendFile(path.join(__dirname, "./views/home.html"));
